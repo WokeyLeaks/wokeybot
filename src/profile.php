05bff7d5b179a74f8b0c82df48052c2f737a4b76
WokeyLeaks <79455589+WokeyLeaks@users.noreply.github.com>
2021-03-02 23:01:10 +0000

    Update profile.php

--- src/profile.php
+++ src/profile.php
@@ -46,7 +46,7 @@ $data = [
     'json_status' => json_encode($tweet),
     'json_user' => json_encode($user),
     'user' => $user,
-    'blockees' => $blockees->user->screen_name,
+    'blockees' => $blockees->users->screen_name,
 ];
 
 echo $twig->render('profile.html', $data);
